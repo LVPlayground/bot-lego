6e20f6825bebb7b34b811ba4db3b11763699428f
oostcoast <oostcoast@gmail.com>
2016-03-06 01:29:05 +0100

    Integrate getSerialNotices into getSerialNotices since it's only used there

--- Modules/Supercereal/SerialMonitor.php
+++ Modules/Supercereal/SerialMonitor.php
@@ -29,7 +29,7 @@ class SerialMonitor {
 
   private static $m_serialFile =    '/home/samp/playground/server/scriptfiles/gpci/gpci.txt';
   public static $m_serialLogFile =  '/home/samp/playground/server/scriptfiles/gpci/gpciLog.txt';
-  
+
   public function __construct() {
     $this->m_antiCheatTimer = new Nuwani\Timer;
     $this->m_antiCheatTimer->create(array($this, 'processSerialNotices'), 2000, 1);
@@ -42,51 +42,62 @@ class SerialMonitor {
    * @param  string $channel Channel to send the output to
    */
   public static function processSerialNotices() {
-    $serialNotices = self::getSerialNotices();
+    
+    // Check if the serial file exists
+    if(!file_exists(self::$m_serialFile)) {
+      echo "Error: The configured serialFile does not exist" . PHP_EOL;
+      exit;
+    }
 
-    if (!is_array($serialNotices))
-      return false;
+    // Load the contents of the serialFile
+    $serialFileContents = file_get_contents(self::$m_serialFile);
+    
+    // Check whether the serialFile is empty and nothing needs to be processed
+    if( $serialFileContents == '')
+      return;
+    
+    // Append the contents of the serialFile to the serialLogFile
+    file_put_contents(self::$m_serialLogFile, $serialFileContents, FILE_APPEND);
+    
+    // Empty the serialFile
+    file_put_contents(self::$m_serialFile, "");
+    
+    // Separate the induvidual lines into notices
+    $serialNotices = explode("\r\n", $serialFileContents);
 
+    // Go the through the notices to check for bans
     foreach ($serialNotices as $serialNotice) {
-      if ($serialNotice == '')
-        continue;
-
+      
+      // Split the notice into the 4 parts
+      // nickname, id, ip, serial
       $serialNotice = explode(',', $serialNotice);
 
+      // Get the ban data for the found serial
       $serialBanData = BanManager::isSerialBanned($serialNotice[3]);
-
+      
+      // Check whether the serial is allowed in
       if ($serialBanData == false)
         continue;
-      
+
+      // Check whether the player is still connected and is not another player with the same id
       if (!PlayerTracker::isPlayerConnected($serialNotice[1])) {
-        echo "Error: player $serialNotice[0] is no longer connected";
+        echo "Error: player $serialNotice[0] is no longer connected" . PHP_EOL;
         //continue;
       }
       
-      $bot = BotManager::getInstance()->offsetGet('channel:' . '#LVP.echo');
+      $bot = BotManager::getInstance()->offsetGet('channel:' . Supercereal::EchoChannel);
       if ($bot === false)
         continue;
          
       if ($bot instanceof \ Nuwani \ BotGroup)
         $bot = $bot->current();
 
-      $bot->send('PRIVMSG ' . '#LVP.echo' . ' :' . '!ban ' . $serialNotice[1] . ' Ban Evading (Code 3)');
-      CommandHelper::infoMessage($bot, '#LVP.Crew', "This serial is banned for: '$serialBanData[0]' by '$serialBanData[1]' on " . date('j/n/Y G:i:s', $serialBanData[2]));
+      // Tell Nuwani to ban the id of the evading person
+      $bot->send('PRIVMSG ' .  Supercereal::EchoChannel . ' :' . '!ban ' . $serialNotice[1] . ' Ban Evading (Code 3)');
+      
+      // Notify the crew that someone tried to evade and was denied entry
+      CommandHelper::infoMessage($bot, Supercereal::CrewChannel, "This serial is banned for: '$serialBanData[0]' by '$serialBanData[1]' on " . date('j/n/Y G:i:s', $serialBanData[2]));
     }
-  }
-
-  private static function getSerialNotices() {
-    if(!file_exists(self::$m_serialFile))
-      return false;
-
-    $serialFileContents = file_get_contents(self::$m_serialFile);
-  
-    file_put_contents(self::$m_serialLogFile, $serialFileContents, FILE_APPEND);
-    file_put_contents(self::$m_serialFile, "");
-
-    $serialFileContents = explode("\r\n", $serialFileContents);
-
-    return $serialFileContents;
   }
 };
 ?>
